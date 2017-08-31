cf0b4e67373b0354256ebbc73e17476f89d077a5
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-08-31 19:12:43 +1000

    Submitting

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -1,2 +1,5 @@
-def add_gigasecond():
-    pass
+from datetime import datetime
+from datetime import timedelta
+
+def add_gigasecond(start: datetime) -> datetime:
+    return start + timedelta(seconds=10**9)
