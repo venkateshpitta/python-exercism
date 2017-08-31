08381ede05c1056dd79eabd193de9c24872f59b3
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-08-31 10:27:09 +1000

    Submitting hamming challenge

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,4 @@
-def distance():
-    pass
+def distance(a: str, b: str) -> int:
+    if len(a)!=len(b):
+        raise ValueError
+    return (0 if a[0]==b[0] else 1) + distance(a[1:], b[1:]) if len(a)>0 else 0
