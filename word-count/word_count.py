489e07b0626391aa1c896954f874f43ed2e711f7
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-08-31 11:51:13 +1000

    Intermediate submission.  Looking for ideas.

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,8 @@
-def word_count():
-    pass
+import re
+from itertools import groupby
+
+def word_count(given: str) -> dict:
+    given = given.replace(',', ' ').replace('_', ' ')
+    words = [re.sub(r'[^a-zA-Z]', '', t) for t in given.lower().split()]
+    words[:] = [word for word in words if word != '' and all(w.isalpha() for w in word)]
+    return {word:len(list(group)) for word, group in groupby(words)}
