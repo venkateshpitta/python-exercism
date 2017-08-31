ad687423f6e003ee4942967406a8df1071e37022
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-01 07:52:42 +1000

    Three more test cases to pass.  Looking for ideas.

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,8 @@
-def decode():
-    pass
+from re import sub
+
+def decode(given: str) -> str:
+    return sub(r'(\d+)(\D)', lambda t: t.group(2) * int(t.group(1)), given)
 
 
-def encode():
-    pass
+def encode(given: str) -> str:
+    return sub(r'(.)\1*', lambda t: str(len(t.group(0))) + t.group(1), given)
