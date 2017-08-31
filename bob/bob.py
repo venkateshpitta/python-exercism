66a218475e8ebd507e8499f6aaa9190e7fad98d8
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-08-31 19:46:38 +1000

    Translated from Java, submitting

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,16 @@
-def hey(string):
-    pass
+def hey(string: str) -> str:
+    out = ""
+    if len(string.strip())==0:
+        out = "Fine. Be that way!"
+    elif string.lower() == string.upper(): ## full of non alpha characters
+        if string.strip()[-1] == "?":
+            out = "Sure."
+        else:
+            out = "Whatever."
+    elif string.upper() == string:
+        out = "Whoa, chill out!"
+    elif string.strip()[-1] == "?":
+        out = "Sure."
+    else:
+        out = "Whatever."
+    return out
