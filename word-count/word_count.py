d1c5d67077f90a3ebf26a02b6ff2d03d9924112b
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-08-31 11:59:17 +1000

    Solution inspired by that of CubicComet

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,8 +1,5 @@
 import re
-from itertools import groupby
 
 def word_count(given: str) -> dict:
-    given = given.replace(',', ' ').replace('_', ' ')
-    words = [re.sub(r'[^a-zA-Z]', '', t) for t in given.lower().split()]
-    words[:] = [word for word in words if word != '' and all(w.isalpha() for w in word)]
-    return {word:len(list(group)) for word, group in groupby(words)}
+    words = "".join(c if c.isalnum() else " " for c in given).lower().split()
+    return {word:words.count(word) for word in set(words)}
