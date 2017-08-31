1f9257fa91a17b7632ff44c57a1cd442fcb8dbc8
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-08-31 10:08:10 +1000

    Submitting

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,6 @@
-def to_rna():
-    pass
+def to_rna(dna: str) -> str:
+    table = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+    rna = ''
+    for n in dna:
+        rna += table[n] if n in table.keys() else ''
+    return rna if len(rna)==len(dna) else ''
