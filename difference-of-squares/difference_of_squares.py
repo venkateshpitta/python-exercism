98afd3203400160cce787247d59ba9583053e7f4
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 11:49:47 +1000

    Passing tests.  Submitting.

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,11 @@
-def square_of_sum():
-    pass
+def square_of_sum(n: int) -> int:
+    s = n * (n + 1) / 2
+    return s**2
 
 
-def sum_of_squares():
-    pass
+def sum_of_squares(n: int) -> int:
+    return n * (n + 1) * (2 * n + 1) / 6
 
 
-def difference():
-    pass
+def difference(n: int) -> int:
+    return square_of_sum(n) - sum_of_squares(n)
