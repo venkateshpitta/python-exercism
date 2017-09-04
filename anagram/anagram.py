f132dd4db98c1018dd7b3c407f25182683af4326
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 17:30:00 +1000

    Passing tests.  Submitting

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,4 @@
-def detect_anagrams(string, array):
-    pass
+from typing import List
+
+def detect_anagrams(string: str, array: List[str]) -> List[str]:
+    return [x for x in array if x.lower() != string.lower() and sorted(x.lower()) == sorted(string.lower())]
