0105cfd45d85050967d2edf6a3dfab4f2387fc77
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 20:13:04 +1000

    Passing tests.  Submitting.

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,18 @@
-def encode(string):
-    pass
+import re
+
+asc = 'abcdefghijklmnopqrstuvwxyz'
+des = asc[::-1]
+asc += '0123456789'
+des += '0123456789'
+e_table = dict(zip(asc, des))
+d_table = dict(zip(des, asc))
+
+def encode(string: str) -> str:
+    pattern = re.compile('[\W_]+')
+    temp = pattern.sub('', string.lower())
+    out = ''.join(e_table[t] for t in temp)
+    return ' '.join(out[i:i+5] for i in range(0, len(out), 5))
 
 
-def decode(string):
-    pass
+def decode(string: str) -> str:
+    return ''.join(d_table[t] for t in string.replace(' ', ''))
