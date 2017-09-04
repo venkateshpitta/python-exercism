e2f00ff6a3fdfc9d0e9f193295a3931614b25720
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 20:30:20 +1000

    Passing tests.  Submitting

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,9 @@
-def sum_of_multiples():
-    pass
+from typing import List
+
+def sum_of_multiples(n: int, ms: List[int]) -> int:
+    s = set()
+    for m in ms:
+        for i in range(1,n):
+            if i%m == 0:
+                s.add(i)
+    return sum(s)
