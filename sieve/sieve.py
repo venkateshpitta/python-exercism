819af596c309d4245bf1b655b3f339aeda652750
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 19:33:29 +1000

    Passing tests.  Submitting.

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,10 @@
-def sieve():
-    pass
+from typing import List
+
+def sieve(n: int) -> List[int]:
+    if n < 2: return []
+    knowns = [2]
+    potentials = list(range(3, n+1, 2))
+    while len(potentials) > 0:
+        knowns.append(potentials[0])
+        potentials = [p for p in potentials[1:] if p%knowns[-1] != 0]
+    return knowns
