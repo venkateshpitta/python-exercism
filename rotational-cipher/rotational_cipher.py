72331210cb1cc08aa0d379dbdac595a2565a9dde
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 11:38:30 +1000

    Passing tests.  Submitting.

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,13 @@
-def rotate():
-    pass
+def rotate(given: str, distance: int) -> str:
+    up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lo = 'abcdefghijklmnopqrstuvwxyz'
+    out = ''
+    for g in given:
+        if g.isalpha():
+            if g.islower():
+                out = out + lo[(lo.index(g)+distance)%26]
+            elif g.isupper():
+                out = out + up[(up.index(g)+distance)%26]
+        else:
+            out = out + g
+    return out
