a33a803154fd0c1effc5ec18e0489ef8c9908506
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-04 18:13:40 +1000

    Passing tests.  Submitting.

--- series/series.py
+++ series/series.py
@@ -1,2 +1,12 @@
-def slices():
-    pass
+from typing import List
+
+def slices(num: str, size: int) -> List[List[int]]:
+    if size == 0: raise ValueError('Cannot have zero length numbers')
+    if size > len(num): raise ValueError('Cannot have slice longer than original number')
+
+    out = []
+    for i in range(0, len(num)-(size-1)):
+        temp = [int(n) for n in num[i:i+size]]
+        out.append(temp)
+
+    return out
