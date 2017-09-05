f990c6f15a0fbd0c3922e4104555cc659f9f7737
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-05 19:28:14 +1000

    Passing tests.  Submitting.

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,3 +1,15 @@
+from typing import Tuple, Sequence
+
 class School(object):
-    def __init__(self):
-        pass
+    def __init__(self, schoolname: str):
+        self.schoolname = schoolname
+        self.register = dict()
+
+    def add(self, student_name: str, grade: int) -> None:
+        self.register.setdefault(grade, []).append(student_name)
+
+    def grade(self, g: int) -> Tuple[str]:
+        return self.register.get(g, [])
+
+    def sort(self) -> Sequence[str]:
+        return [(g, tuple(sorted(self.register[g]))) for g in sorted(self.register)]
