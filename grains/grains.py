cf8d25f8bae732537dc57f32a661289fb6d9ac30
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-05 20:43:32 +1000

    Passing tests.  Submitting.

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,8 @@
-def on_square():
-    pass
+def on_square(n: int) -> int:
+    if n < 1 or n > 64: raise ValueError
+    return 2**(n-1)
 
 
-def total_after():
-    pass
+def total_after(n: int) -> int:
+    if n < 1 or n > 64: raise ValueError
+    return (1 - 2**n) // (1 - 2)
