dafa29d76960279c893fdc3c4318541a6109eb77
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-05 19:51:21 +1000

    Passing tests.  Submitting.

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,2 +1,9 @@
-def flatten():
-    pass
+def flatten(nested_list): ## type hinting here can be interesting
+    def helper():
+        for item in nested_list:
+            if isinstance(item, (list, tuple)):
+                for subitem in flatten(item):
+                    yield subitem
+            elif item is not None:
+                yield item
+    return list(helper())
