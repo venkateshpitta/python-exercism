efdc54bf6cf188e7de07787c14eedc18f9d13b0b
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-06 09:43:35 +1000

    Passing tests.  Submitting.

--- etl/etl.py
+++ etl/etl.py
@@ -1,2 +1,9 @@
-def transform():
-    pass
+from typing import Dict, List
+
+def transform(given: Dict[int, List[str]]) -> Dict[str, int]:
+    out = dict()
+    for score, letters in given.items():
+        for letter in letters:
+            out[letter.lower()] = score
+
+    return out
