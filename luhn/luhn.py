8a121dcb450ce25480b4ca1e269fa428876c179c
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-05 21:14:28 +1000

    Passing tests.  Submitting.

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,3 +1,10 @@
 class Luhn(object):
-    def __init__(self):
-        pass
+    def __init__(self, num: str):
+        self.cc_num = num.strip().replace(' ', '')
+
+    def is_valid(self) -> bool:
+        if not self.cc_num.isdigit() or len(self.cc_num) <= 1: return False
+        rev = self.cc_num[::-1]
+        odds = [int(rev[r]) for r in range(0, len(rev), 2)]
+        evens = [2*int(rev[r]) - (9 if 2*int(rev[r])>9 else 0) for r in range(1, len(rev), 2)]
+        return (sum(odds) + sum(evens))%10 == 0
