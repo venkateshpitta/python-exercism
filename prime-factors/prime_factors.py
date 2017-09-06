ca7f446e482e008dd56ea9fc29dce30a620560b2
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-06 13:17:35 +1000

    Passing tests.  Submitting.

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -1,2 +1,11 @@
-def prime_factors():
-    pass
+from typing import List
+import prime_tools ## pip install prime_tools
+
+def prime_factors(n: int) -> List[int]:
+    pith = prime_tools.prime_factorization.list_prime_factors(n)
+    out = []
+    for prime in pith:
+        while n%prime == 0:
+            out.append(prime)
+            n //= prime
+    return out
