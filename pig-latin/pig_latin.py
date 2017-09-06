97afb45cadd32bb9b6e36bc2af3bafd0f3421bb3
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-06 14:07:18 +1000

    I am impressed.  Feels so easy to translate from Clojure.

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,20 @@
-def translate():
-    pass
+from functools import partial
+
+def process_word(word: str) -> str:
+    normal_vowels, cluster_vowels = 'aeiou', ['yt', 'xr']
+    twos_prefix, threes_prefix = ['ch', 'qu', 'th'], ['thr', 'sch']
+    pfn = partial(word.startswith)
+    ayed = lambda t: t+'ay'
+
+    if any(map(pfn, normal_vowels)) or any(map(pfn, cluster_vowels)):
+        return ayed(word)
+    elif any(map(pfn, threes_prefix)) or (word[1:].startswith('qu') and
+                                        not any(map(pfn, normal_vowels))):
+        return ayed(word[3:]+word[:3])
+    elif any(map(pfn, twos_prefix)):
+        return ayed(word[2:]+word[:2])
+    else:
+        return ayed(word[1:]+word[:1])
+
+def translate(sentence: str) -> str:
+    return ' '.join(process_word(word) for word in sentence.split(' '))
