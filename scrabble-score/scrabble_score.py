0286d4520809a74cbf08f50fad362e81fc49baef
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-07 12:09:40 +1000

    Passing tests.  Submitting

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,7 @@
-def score():
-    pass
+import string
+
+def score(given: str) -> int:
+    letters = string.ascii_lowercase
+    numbers = 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+    table = dict(zip(letters, numbers))
+    return sum(table[g] for g in given.lower()) if len(given) > 0 else 0
