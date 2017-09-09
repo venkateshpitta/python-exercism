335b4982ef531c08540af55fe39d7c31e69e7912
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-09 13:37:05 +1000

    Passing tests.

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,2 +1,10 @@
-def check_lists():
-    pass
+from typing import List
+
+SUBLIST, SUPERLIST, EQUAL, UNEQUAL = range(4)
+
+def check_lists(l1, l2): ## type hinting here will be interesting
+    s1, s2 = '_'.join(str(i) for i in l1), '_'.join(str(i) for i in l2)
+    if s1 == s2: return EQUAL
+    elif s1 in s2: return SUBLIST
+    elif s2 in s1: return SUPERLIST
+    else: return UNEQUAL
