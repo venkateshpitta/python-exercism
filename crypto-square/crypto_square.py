835d95918d65f431df4a88bc172f08634ff465bd
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-09 12:20:27 +1000

    Passing tests.  Submitting.

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,2 +1,8 @@
-def encode():
-    pass
+import math
+
+def encode(given: str) -> str:
+    if len(given)==0: return given
+    cleansed = [g for g in given.lower() if g.isalnum()]
+    width = int(math.ceil(len(cleansed)**0.5))
+    words = [''.join(cleansed[i::width]) for i in range(width)]
+    return ' '.join(words)
