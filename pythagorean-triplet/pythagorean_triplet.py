8a7113b84c8bfc77cef842c728f88c5a1cc28bfc
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-10 00:10:27 +1000

    Passing is_triplet

--- pythagorean-triplet/pythagorean_triplet.py
+++ pythagorean-triplet/pythagorean_triplet.py
@@ -1,3 +1,7 @@
+import math
+from typing import Tuple
+Triplet = Tuple[int, int, int]
+
 def primitive_triplets():
     pass
 
@@ -6,5 +10,5 @@ def triplets_in_range():
     pass
 
 
-def is_triplet():
-    pass
+def is_triplet(t: Triplet) -> bool:
+    return 1 == math.gcd(t[0], math.gcd(t[1], t[2]))
