2d8b778a220b485fad80428bf4491f6ab476ef19
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-13 10:27:18 +1000

    Looking for ideas

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,19 @@
+import random ## choices found in 3.6, not in 3.5
+import string
+
 class Robot(object):
     def __init__(self):
-        pass
+        self._name = self._new_name()
+
+    def _new_name(self):
+        # return random.choices(string.ascii_uppercase, k=2) + random.choices(string.digits, k=3)
+        return random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) +\
+            random.choice(string.digits) + random.choice(string.digits) + random.choice(string.digits)
+
+    @property
+    def name(self):
+        return self._name
+
+    def reset(self):
+        self._name = self._new_name()
+        return self._name
