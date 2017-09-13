e9c713aa99dd64ffe7a0402c1a5c9a96ed45e562
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-13 10:58:05 +1000

    Passing tests.  Submitting.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,3 +1,5 @@
 class Matrix(object):
     def __init__(self, given: str):
         self.given = given
+        self.rows = [[int(i) for i in row.split()] for row in self.given.split('\n')]
+        self.columns = [list(r) for r in zip(*self.rows)]
