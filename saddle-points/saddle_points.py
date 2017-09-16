b0013dfe7c262b8d9d0b23cf01d0e32cae19d432
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-16 13:34:50 +1000

    Passing tests.  Submitting.

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,2 +1,13 @@
-def saddle_points():
-    pass
+from typing import List, Set, Tuple
+
+def saddle_points(given: List[List[int]]) -> Set[Tuple[int, int]]:
+    if not all(len(given[0])==len(row) for row in given[1:]):
+        raise ValueError("Non-uniform row sizes detected.  Aborting.")
+
+    transposed = [list(c) for c in zip(*given)]
+    out = []
+    for ri, rv in enumerate(given):
+        for ci, cv in enumerate(transposed):
+            if max(rv) == min(cv) == given[ri][ci]:
+                out.append((ri, ci))
+    return set(out)
