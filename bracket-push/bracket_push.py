52243d6931d2cfed85a372c0caeb7891cbdd286f
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-21 12:57:54 +1000

    Passing all tests.  Submitting.

--- bracket-push/bracket_push.py
+++ bracket-push/bracket_push.py
@@ -1,2 +1,18 @@
-def check_brackets(string):
-    pass
+def check_brackets(string: str) -> bool:
+    all_brackets = {'}': '{', ']': '[', ')': '('}
+    open_brackets = set(all_brackets.values())
+    close_brackets = set(all_brackets.keys())
+
+    if len(string) == 0:
+        return True
+
+    stack = []
+    for i in range(len(string)):
+        if string[i] in open_brackets:
+            stack.append(string[i])
+        elif string[i] in close_brackets:
+            if len(stack) > 0 and stack[-1] == all_brackets[string[i]]:
+                stack.pop()
+            else: return False
+
+    return len(stack) == 0
