2918046088b9932509f0c5a9020f1c712e395f38
Venkateshpitta@gmail.com <Venkatesh Pitta>
2017-09-29 16:38:19 +1000

    What is exp, looking at others for ideas

--- complex-numbers/complex_numbers.py
+++ complex-numbers/complex_numbers.py
@@ -1,24 +1,36 @@
+from typing import Union
+
+number = Union[int, float]
+
 class ComplexNumber(object):
-    def __init__(self, real, imaginary):
-        pass
+    def __init__(self, real: number, imaginary: number) -> None:
+        self.real, self.imaginary = real, imaginary
 
     def add(self, other):
-        pass
+        assert(isinstance(other, ComplexNumber))
+        return ComplexNumber((self.real + other.real), (self.imaginary + other.imaginary))
 
     def mul(self, other):
-        pass
+        assert(isinstance(other, ComplexNumber))
+        return ComplexNumber((self.real * other.real - self.imaginary * other.imaginary),
+                             (self.real * other.imaginary + self.imaginary * other.real))
 
     def sub(self, other):
-        pass
+        assert(isinstance(other, ComplexNumber))
+        return ComplexNumber((self.real - other.real), (self.imaginary - other.imaginary))
 
     def div(self, other):
-        pass
+        assert(isinstance(other, ComplexNumber))
+        den = other.real**2 + other.imaginary**2
+        r = self.real * other.real + self.imaginary * other.imaginary
+        i = self.imaginary * other.real - self.real * other.imaginary
+        return ComplexNumber((r/den), (i/den))
 
-    def abs(self):
-        pass
+    def abs(self) -> number:
+        return (self.real ** 2 + self.imaginary ** 2) ** 0.5
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, -1*self.imaginary)
 
     def exp(self):
         pass
